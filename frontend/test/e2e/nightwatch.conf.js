34c6b0b533cdd5e3036874dcd806bd228b404280
Martin Fracker <martin.frackerjr@gmail.com>
2017-09-02 15:45:39 -0500

    Properly configure night watch?

--- frontend/test/e2e/nightwatch.conf.js
+++ frontend/test/e2e/nightwatch.conf.js
@@ -1,16 +1,48 @@
 require('babel-register')
 var config = require('../../config')
 
+var testSettings = {
+  default: {
+    silent: true,
+    globals: {
+      devServerURL: 'http://localhost:' + (process.env.PORT || config.dev.port)
+    }
+  },
+
+  chrome: {
+    desiredCapabilities: {
+      browserName: 'chrome',
+      javascriptEnabled: true,
+      acceptSslCerts: true
+    }
+  },
+
+  firefox: {
+    desiredCapabilities: {
+      browserName: 'firefox',
+      javascriptEnabled: true,
+      acceptSslCerts: true
+    }
+  }
+}
 var selenium = {}
 if (process.env.REMOTE_E2E) {
-  console.log('Browerstack selenium')
+  console.log('remote')
   selenium = {
     start_process: false,
     host: 'hub-cloud.browserstack.com',
-    'port': 80
+    port: 80
   }
+
+  Object.assign(testSettings.default, {
+    desiredCapabilities: {
+      'browserstack.user': process.env.BROWSERSTACK_USER,
+      'browserstack.key': process.env.BROWSERSTACK_ACCESS_KEY,
+      'browser': 'chrome'
+    }
+  })
 } else {
-  console.log('Regular selenium')
+  console.log('REGULAR')
   selenium = {
     start_process: true,
     server_path: require('selenium-server').path,
@@ -20,48 +52,20 @@ if (process.env.REMOTE_E2E) {
       'webdriver.chrome.driver': require('chromedriver').path
     }
   }
+
+  Object.assign(testSettings.default, {
+    selenium_port: 4444,
+    selenium_host: 'localhost'
+  })
 }
+console.log(JSON.stringify(selenium, null, 2))
+console.log(JSON.stringify(testSettings, null, 2))
 
 // http://nightwatchjs.org/gettingstarted#settings-file
 module.exports = {
   src_folders: ['test/e2e/specs'],
   output_folder: 'test/e2e/reports',
   custom_assertions_path: ['test/e2e/custom-assertions'],
-
-  selenium: selenium,
-
-  test_settings: {
-    default: {
-      selenium_port: 4444,
-      selenium_host: 'localhost',
-      silent: true,
-      globals: {
-        devServerURL: 'http://localhost:' + (process.env.PORT || config.dev.port)
-      }
-    },
-
-    chromeRemote: {
-      desiredCapabilities: {
-        'browserstack.user': process.env.BROWSERSTACK_USER,
-        'browserstack.key': process.env.BROWSERSTACK_ACCESS_KEY,
-        'browser': 'chrome'
-      }
-    },
-
-    chrome: {
-      desiredCapabilities: {
-        browserName: 'chrome',
-        javascriptEnabled: true,
-        acceptSslCerts: true
-      }
-    },
-
-    firefox: {
-      desiredCapabilities: {
-        browserName: 'firefox',
-        javascriptEnabled: true,
-        acceptSslCerts: true
-      }
-    }
-  }
+  selenium,
+  test_settings: testSettings
 }
