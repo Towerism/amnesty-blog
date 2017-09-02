53e9fa7eb0c9d3e9db11baca704b23a1d2777b64
Martin Fracker <martin.frackerjr@gmail.com>
2017-09-02 13:01:48 -0500

    Change integration from being all caps

--- ci/install.sh
+++ ci/install.sh
@@ -5,7 +5,7 @@ if [ "${SERVICE}" == "api" ]; then
   cp ci/testAuth.json api/config/auth.json
 fi
 
-if [ "${SERVICE}" != "INTEGRATION" ]; then
+if [ "${SERVICE}" != "integration" ]; then
   (cd $SERVICE && yarn)
 else
   (cd api && yarn)
